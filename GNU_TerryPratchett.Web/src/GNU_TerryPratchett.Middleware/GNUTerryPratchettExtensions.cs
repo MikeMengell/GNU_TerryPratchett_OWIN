7d1ffc9d168149084526a51c6ec4c13d48bd8d27
MikeMengell <mikemengell@hotmail.com>
2015-03-18 09:33:05 +0000

    Middleware + Site

---
+++ GNU_TerryPratchett.Web/src/GNU_TerryPratchett.Middleware/GNUTerryPratchettExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Builder;
+using System;
+
+namespace GNU_TerryPratchett.Middleware
+{
+    /// <summary>
+    /// IApplicationBuilder extensions for the GNUTerryPratchettExtensions.
+    /// </summary>
+    public static class GNUTerryPratchettExtensions
+    {
+        /// <summary>
+        /// Adds the GNUTerryPratchettMiddleware to the pipeline with the given options.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseGNUTerryPratchett(this IApplicationBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException("builder");
+            }
+
+            return builder.Use(next => new GNUTerryPratchettMiddleware(next).Invoke);
+        }
+    }
+}
